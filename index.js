2ed4d1f923b442373fad558b1eebcf0dcf617822
Passkey-md <167205077+Passkey-md@users.noreply.github.com>
2024-07-15 00:52:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//base by DGXeon x AkmalMods
-//YouTube: @AkmalMods
-//Instagram: akmal_mods
-//Telegram: t.me/Darkfamly
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@AkmalMods
+//base by DGXeon x Phantom
+//YouTube: @phantom-hacks01
+//Instagram: phantom-hacks01
+//Telegram: t.me/Hackerzone25
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@phantom-hacks01?si=u0_XNt7-muu6TJGY
 
 const {
    spawn
